85645195fb69b55ee8e897af54a3fc06985604e8
Shahariar Imtiaz <shahariar07@hotmaail.com>
2026-09-26 22:10:09 +0600

    feat: add project URL configuration and new product images, and remove datadump.json

--- pickpickles_project/urls.py
+++ pickpickles_project/urls.py
@@ -12,10 +12,8 @@ urlpatterns = [
     path('dashboard/', include('dashboard.urls')),
     path('', include('store.urls')),
     re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATICFILES_DIRS[0] if settings.STATICFILES_DIRS else settings.STATIC_ROOT}),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 # Custom Error Handlers
 handler404 = 'store.views.custom_404_view'
